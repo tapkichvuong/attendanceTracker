73b3cde62ed7fc9e3a706a69e885a636756e1d27
Truong Minh Huy <huytruongminh44@gmail.com>
2024-01-08 21:26:38 +0700

    fix api

--- Attendance/src/main/java/com/ATC/Attendance/service/SessionService.java
+++ Attendance/src/main/java/com/ATC/Attendance/service/SessionService.java
@@ -28,11 +28,11 @@ public class SessionService {
     }
 
 
-    //INSERT INTO subject (subject_code, subject_name) VALUES ('CT202', 'Nguyen li may hoc');
-    //INSERT INTO lesson (id, lesson_name, subject_code) VALUES (1, 'Gioi thieu may hoc', 'CT202');
-    //INSERT INTO teacher (teacher_code, teacher_name) VALUES ('12345', 'Nguyen Van A');
-    //INSERT INTO session (is_active, sid, lesson_id, time_end, time_start, teacher_code)
-    //              VALUES (false, 1, 1, '2024-01-01 10:00:00', '2024-01-01 09:00:00', '12345');
+//    INSERT INTO subject (subject_code, subject_name) VALUES ('CT202', 'Nguyen li may hoc');
+//    INSERT INTO lesson (lid, lesson_name, subject_code) VALUES (1, 'Gioi thieu may hoc', 'CT202');
+//    INSERT INTO teacher (teacher_code, teacher_name) VALUES ('12345', 'Nguyen Van A');
+//    INSERT INTO session (is_active, sid, lesson_id, time_end, time_start, teacher_code)
+//                  VALUES (false, 1, 1, '2024-01-01 10:00:00', '2024-01-01 09:00:00', '12345');
     public ActiveSessionRes activeSession(ActiveSessionReq sessionRequest) {
         try {
 
@@ -87,13 +87,13 @@ public class SessionService {
 //      ('S003', 'https://example.com/student3.jpeg', 'Charlie Lee'),
 //      ('S004', 'https://example.com/student4.ico', 'David Brown'),
 //      ('S005', 'https://example.com/student5.gif', 'Emily Jones');
-
+//
 //    INSERT INTO session (is_active, sid, lesson_id, time_end, time_start, teacher_code)
 //    VALUES
 //            (false, 2, 1, '2024-01-02 10:00:00', '2024-01-02 09:00:00', '12345'),
 //            (false, 3, 1, '2024-01-03 10:00:00', '2024-01-03 09:00:00', '12345'),
 //            (false, 4, 1, '2024-01-04 10:00:00', '2024-01-04 09:00:00', '12345');
-
+//
 //      INSERT INTO regSubject (student_code, subject_code)
 //      VALUES
 //            ('S001', 'CT202'),
@@ -101,7 +101,7 @@ public class SessionService {
 //            ('S003', 'CT202'),
 //            ('S004', 'CT202'),
 //            ('S005', 'CT202');
-
+//
 //INSERT INTO Attendances_table(student_code, session_id)
 //    VALUES
 //            ('S001', 1),
