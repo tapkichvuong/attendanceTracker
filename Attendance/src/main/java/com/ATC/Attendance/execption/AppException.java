03b7defa7038783893f3bb4c116e6a70df4506e1
Khoi <pdkhoi20@apcs.fitus.edu.vn>
2024-01-08 15:13:25 +0700

    throw exception in login and regist

--- Attendance/src/main/java/com/ATC/Attendance/execption/AppException.java
+++ Attendance/src/main/java/com/ATC/Attendance/execption/AppException.java
@@ -1,13 +1,14 @@
 package com.ATC.Attendance.execption;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AppException extends RuntimeException{
-    private int code;
     private String message;
 }
--- Attendance/src/main/java/com/ATC/Attendance/service/AuthenticationService.java
+++ Attendance/src/main/java/com/ATC/Attendance/service/AuthenticationService.java
@@ -1,15 +1,21 @@
 package com.ATC.Attendance.service;
 
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ATC.Attendance.dto.AuthenticationRequest;
 import com.ATC.Attendance.dto.AuthenticationResponse;
 import com.ATC.Attendance.dto.RegisterRequest;
 import com.ATC.Attendance.entities.UserEntity;
+import com.ATC.Attendance.execption.AppException;
 import com.ATC.Attendance.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -24,19 +30,35 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    public AuthenticationResponse authenticate(AuthenticationRequest request){
-        authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(request.getUserCode(),request.getPassword())
-        );
-        var user = repository.findByUserCode(request.getUserCode()).orElseThrow();
-        var jwtToken = jwtService.generateToken(user);
-        return AuthenticationResponse.builder().token(jwtToken).build();
-    }
+    // public AuthenticationResponse authenticate(AuthenticationRequest request){
+    //     authenticationManager.authenticate(
+    //         new UsernamePasswordAuthenticationToken(request.getUserCode(),request.getPassword())
+    //     );
+    //     var user = repository.findByUserCode(request.getUserCode()).orElseThrow();
+    //     var jwtToken = jwtService.generateToken(user);
+    //     return AuthenticationResponse.builder().token(jwtToken).build();
+    // }
     
     public AuthenticationResponse register(RegisterRequest request){
+        Optional<UserEntity> existingUser = repository.findByUserCode(request.getUserCode());
+        if(existingUser.isPresent()){
+            return AuthenticationResponse.builder().message("user already exist").build();
+        }
         var user = UserEntity.builder().userCode(request.getUserCode()).userPassword(passwordEncoder.encode(request.getPassword())).role(request.getRole().toUpperCase()).build();
         repository.save(user);
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder().token(jwtToken).build();
     }
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
+    try {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(request.getUserCode(),request.getPassword())
+        );
+    } catch (BadCredentialsException e) {
+         return AuthenticationResponse.builder().message("Invalid username or password").build();
+    }
+    var user = repository.findByUserCode(request.getUserCode()).orElseThrow();
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder().token(jwtToken).build();
+}
 }
